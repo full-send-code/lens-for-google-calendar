0a7ac6512d5dcfb1c58fa9c03ea3b8d073760561
Callen Barton <7515844+cal5barton@users.noreply.github.com>
2025-10-29 21:52:48 -0600

    Update logger.js with new logger configuration

---
+++ src/logger.js
@@ -0,0 +1,1 @@
+{"name": "Lens Logger", "version": "1.0.0", "description": "Logger system for Lens for Google Calendar", "main": "src/logger.js", "scripts": {"test": "echo \"Error: no test specified\" && exit 1"}, "keywords": [], "author": "", "license": "ISC", "dependencies": {}, "devDependencies": {}}
